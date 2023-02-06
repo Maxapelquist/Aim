6f8f92803b1a67eba5ae01b908c6e067d589a1a6
Maxapelquist <94568997+Maxapelquist@users.noreply.github.com>
2023-02-06 20:41:14 +0100

    gg
    allt möjligtvg

---
+++ JS/toolbar.js
@@ -0,0 +1,14 @@
+const contactButton = document.getElementById("contact-button");
+contactButton.addEventListener("click", function () {
+    window.open("contact.html", "_self");
+});
+
+const profileButton = document.getElementById("profile-button");
+profileButton.addEventListener("click", function () {
+    window.open("profile.html", "_self");
+});
+
+const aboutButton = document.getElementById("about-button");
+aboutButton.addEventListener("click", function () {
+    window.open("about.html", "_self");
+});
