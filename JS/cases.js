bea876a7fe72861eeb6b74733b7cbd5dbff16b51
Maxapelquist <94568997+Maxapelquist@users.noreply.github.com>
2023-02-06 20:56:48 +0100

    design, frontpage

--- JS/cases.js
+++ JS/cases.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(function(box) {
   box.addEventListener("click", function () {
-    window.open("sheet.html", "_self");
+    window.open("../JS/sheet.js", "_self");
   });
 });
